21a5ac22d2ee6fe9594422cc979d421c94e5302f
Roy <royroy.euph@gmail.com>
2019-12-10 02:12:05 +0900

    リンク切れテストを修正

--- tests/test_url_link.py
+++ tests/test_url_link.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-import random
 import sys
+import os
 import subprocess
 import glob
 import re
@@ -15,20 +15,25 @@ import aiohttp
 
 def test_urls():
 	"""
-	urlをダウンロードして、bs4を解析して
+	docs配下のMarkdownファイルを対象として
 	全リンクのステータスチェックする
 	"""
 
-	base_url = "http://localhost:8000/"
+	# mkdocs serve実行時のURL
+	mkdocs_url = 'http://127.0.0.1:8000/'
+	
+	# 並列実行数
+	max_access_count = 100
+
 	proc = None
 
 	if sys.platform == 'win32':
 		asyncio.set_event_loop(asyncio.ProactorEventLoop())
 
 	proc = None
-	# Serverが起動しているかチェック
 	try:
-		requests.get(base_url)
+		# mkdocs serveが起動しているかチェック
+		requests.get(mkdocs_url)
 	except requests.exceptions.ConnectionError:
 		# 接続できない場合はmkdocs serveを起動
 		proc = subprocess.Popen(['mkdocs', 'serve'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
@@ -36,58 +41,73 @@ def test_urls():
 			if 'Start detecting changes' in proc.stdout.readline():
 				break
 
+	# テスト対象URLのリスト
 	target_urls = []
+	# docs配下の*.mdファイルを検索し、対象URLリストを作成
 	for md in glob.glob('docs/**/*.md', recursive=True):
-		target_urls.append(base_url + re.sub(r'^docs\\|\.md$|index\.md$', '', md).replace('\\', '/'))
+		target_urls.append(mkdocs_url + re.sub(r'^docs\\|\.md$|index\.md$', '', md).replace(os.sep, '/'))
 
-	test_urls = []
+	check_urls = []
 	errors = []
-	def extend_test_urls(url_list):
-		test_urls.extend(url_list)
+	# 成功時コールバック関数
+	def extend_test_urls(future):
+		check_urls.extend(future.result())
 
+	# エラー時コールバック関数
 	def set_error(result):
 		errors.append(result)
 
+	# 並列度制限用セマフォ
+	sem = asyncio.Semaphore(max_access_count)
+
 	# パース対象URLからリンクを取得
 	asyncio.get_event_loop().run_until_complete(
-		asyncio.wait([
-			request_and_parse(target_url, extend_test_urls, set_error) for target_url in target_urls
-		])
+		fetch_and_parse(target_urls, extend_test_urls, set_error, sem)
 	)
 	assert len(errors) == 0, errors
 	
 	# 重複排除
-	test_urls = list(set(test_urls))
+	check_urls = list(set(check_urls))
 
 	errors = []
 	# リンクの生死チェック(非同期実行)
 	asyncio.get_event_loop().run_until_complete(
-		asyncio.wait([
-			check_url(test_url, set_error) for test_url in test_urls
-		])
+		check_url(check_urls, set_error, sem)
 	)
+	for err in errors:
+		print(err, file=sys.stderr)
 	assert len(errors) == 0, errors
 
+	# mkdocs serveを起動した場合は、プロセスを終了させる
 	if proc is not None:
 		proc.kill()
 		proc = None
 
-async def request_and_parse(target_url, result_callback, error_callback):
+async def fetch_and_parse(target_urls, result_callback, error_callback, semaphore):
 	"""
-	html文字列からリンクを取り出してリンクの一覧を返す
-	:param host: 対象とするホスト名
-	:param html: パース対象のHTMLテキスト
+	URLからHTMLを取得し、リンクの一覧を抽出してコールバックに渡す
+	:param target_urls: 対象とするURLのリスト
+	:param result_callback: 成功時のリンクを渡すコールバック
+	:param error_callback: エラー時のコールバック
 	"""
-	async with aiohttp.ClientSession() as session:
-		async with session.get(target_url) as response:
-			# 指定されたURLをGETして解析
-			status_code = response.status
-			if status_code == 200:
-				result_callback(get_links(target_url, await response.text()))
-			else:
-				error_callback('{}:{}'.format(str(status_code), target_url))
+	tasks = []
+	async def fetch(s, url):
+		async with semaphore:
+			async with s.get(url) as res:
+				# 指定されたURLをGETして解析
+				status_code = res.status
+				if status_code != 200:
+					error_callback('{}:{}'.format(str(status_code), url))
+				return await get_links(url, await res.text())
 
-def get_links(target_url, html) -> list:
+	async with aiohttp.ClientSession() as session:
+		for target_url in target_urls:
+			task = asyncio.ensure_future(fetch(session, target_url))
+			task.add_done_callback(result_callback)
+			tasks.append(task)
+		return await asyncio.wait(tasks)
+
+async def get_links(target_url, html) -> list:
 	soup = BeautifulSoup(html, "lxml")
 	test_urls = []
 	for a in soup.find_all("a"):
@@ -96,15 +116,17 @@ def get_links(target_url, html) -> list:
 		url = urlparse(target_url)
 		if '://' in href:
 			if url.scheme == 'http' or url.scheme == 'https':
-				if url.netloc in href:
+				if url.netloc in href and len(url.netloc) > 0:
 					# 絶対リンクかつ、同一ドメイン
-					test_urls.append(quote(href), safe='/:', encoding='utf-8')
+					test_urls.append(quote(href, safe='./:', encoding='utf-8'))
 				else:
 					# 外部サイトリンクはテストしない
-					print('IGNORE:{}'.format(href))
+					#print('IGNORE:{}'.format(href))
+					pass
 			else:
 				# http, https以外はテストしない
-				print('IGNORE:{}'.format(href))
+				#print('IGNORE:{}'.format(href))
+				pass
 		elif href[0] == '#':
 			# フラグメント指定は無視
 			pass
@@ -117,22 +139,35 @@ def get_links(target_url, html) -> list:
 			else:
 				path = url.path + '/'
 
-			test_urls.append(quote('{}://{}{}{}'.format(url.scheme, url.netloc, path, href), safe='/:', encoding='utf-8'))
+			test_urls.append(quote('{}://{}{}{}'.format(url.scheme, url.netloc, path, href), safe='./:', encoding='utf-8'))
 
 	# 重複排除
 	test_urls = list(set(test_urls))
 	return test_urls
 
 
-async def check_url(url, error_callback):
+async def check_url(check_urls, error_callback, semaphore):
 	"""
 	非同期でURLをチェックして、HTTP STATUSが200を応答することをチェック
-	:param url: str
+	:param check_urls: str
+	:param error_callback: Func
 	"""
+	tasks = []
+	async def check(s, url):
+		async with semaphore:
+			try:
+				async with s.get(url) as res:
+					# 指定されたURLをGETして解析
+					status_code = res.status
+					if status_code != 200:
+						error_callback('{}:{}'.format(str(status_code), url))
+			except:
+				error_callback('{}:{}'.format(sys.exc_info() ,url))
+
 	async with aiohttp.ClientSession() as session:
-		async with session.get(url) as response:
-			status_code = response.status
-			if status_code != 200:
-				error_callback('{}:{}'.format(str(status_code), url))
+		for url in check_urls:
+			task = asyncio.ensure_future(check(session, url))
+			tasks.append(task)
+		return await asyncio.wait(tasks)
 
 test_urls()
